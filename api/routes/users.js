5fd3a8c56fec99ef10d4084f37312603d954b89e
Anton <56392640+AntonBertrand@users.noreply.github.com>
2023-05-22 17:02:01 +0100

    Added Room Controller and Routes
    Ability to create/update/delete/get rooms and admin verification

--- api/routes/users.js
+++ api/routes/users.js
@@ -4,7 +4,7 @@ import { verifyUser, verifyToken, verifyAdmin} from "../utils/verifyToken.js";
 
 const router = express.Router();
 
-router.get("/checkauthentication", verifyToken, (req,res,next) => {
+/* router.get("/checkauthentication", verifyToken, (req,res,next) => {
     res.send("hello user, you are logged in")
 })
 
@@ -16,21 +16,21 @@ router.get("/checkuser/:id", verifyUser, (req,res,next) => {
 
 router.get("/checkadmin/:id", verifyAdmin, (req,res,next) => {
     res.send("hello admin, you are logged in and you can delete all accounts")
-})
+}) */
 
 // UPDATE
-router.put("/:id", updateUser);
+router.put("/:id", verifyUser, updateUser);
 
 
 // DELETE
-router.delete("/:id", deleteUser);
+router.delete("/:id", verifyUser, deleteUser);
 
 
 // GET
-router.get("/:id", getUser);
+router.get("/:id", verifyUser, getUser);
 
 
 // GET ALL
-router.get("/", getUsers);
+router.get("/", verifyAdmin, getUsers);
 
 export default router;
