0a7b65cc6647218a14b58e709fde02cdc089e9cf
Anton <56392640+AntonBertrand@users.noreply.github.com>
2023-06-07 18:45:06 +0100

    City Tiles Buttons now work
    City tiles on homepage now link to a link of accommodations available in the selected city

--- client/src/components/featured/Featured.jsx
+++ client/src/components/featured/Featured.jsx
@@ -1,16 +1,63 @@
 import React from 'react'
 import "./featured.css"
 import useFetch from '../../hooks/useFetch'
+import { useState, useEffect } from 'react'
+import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { SearchContext } from "../../context/SearchContext";
+import { useRef } from 'react';
 
 export const Featured = () => {
 
+    const renderCount = useRef(0);
+
     const {data, loading, error} = useFetch("/hotels/countByCity?cities=berlin,madrid,london");
 
+    const { dispatch } = useContext(SearchContext);
+
+    const navigate = useNavigate();
+
+    const [options, setOptions] = useState({
+      adult: 1,
+      children: 0,
+      room: 1,
+    });
+
+    const [destination, setDestination] = useState("");
+    const [dates, setDates] = useState([
+        {
+          startDate: new Date(),
+          endDate: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+        }
+      ]);
+
+
+    const handleSearch = () => {
+
+      setDestination("berlin");
+
+      console.log({state:{destination,dates,options}});
+
+    dispatch({ type: "NEW_SEARCH", payload: { destination, dates, options } });
+    navigate("/hotels", {state:{destination,dates,options}} )
+  } 
+
+  useEffect(() => {
+
+    renderCount.current = renderCount.current + 1;
+
+    if (renderCount.current < 3) {
+      return; // 👈️ return early if initial render
+    } else {
+      handleSearch()
+    }
+
+  }, [destination])
 
   return (
     <div className='featured'>
         { loading ? ("Loading please wait" ) : ( <> <div className="featuredItem">
-            <img src="https://cf.bstatic.com/xdata/images/city/600x600/976952.jpg?k=4c536b3833e5f6721ff5f89fa4f15d8f50fd5cd4a963060414078428aa67a6d5&o=" alt="" className='featuredImg'/>
+            <img src="https://cf.bstatic.com/xdata/images/city/600x600/976952.jpg?k=4c536b3833e5f6721ff5f89fa4f15d8f50fd5cd4a963060414078428aa67a6d5&o=" alt="" className='featuredImg' onClick={() => setDestination("berlin")}/>
             <div className="featuredTitles">
                 <h1>Berlin</h1>
                 <h2>{data[0]} Properties</h2>
@@ -18,7 +65,7 @@ export const Featured = () => {
         </div>
 
         <div className="featuredItem">
-            <img src="https://cf.bstatic.com/xdata/images/city/600x600/976980.jpg?k=29c289ca126b54f55ca35b7c11393b93106a7305a44f60344028511a6d404092&o=" alt="" className='featuredImg'/>
+            <img src="https://cf.bstatic.com/xdata/images/city/600x600/976980.jpg?k=29c289ca126b54f55ca35b7c11393b93106a7305a44f60344028511a6d404092&o=" alt="" className='featuredImg' onClick={() => setDestination("madrid")} />
             <div className="featuredTitles">
                 <h1>Madrid</h1>
                 <h2>{data[1]} Properties</h2>
@@ -26,7 +73,7 @@ export const Featured = () => {
         </div>
 
         <div className="featuredItem">
-            <img src="https://cf.bstatic.com/xdata/images/city/600x600/613088.jpg?k=a370ac3fb385fb211b35a79a42b0e968ddb458be37108af476c558bf4cedc1f3&o=" alt="" className='featuredImg'/>
+            <img src="https://cf.bstatic.com/xdata/images/city/600x600/613088.jpg?k=a370ac3fb385fb211b35a79a42b0e968ddb458be37108af476c558bf4cedc1f3&o=" alt="" className='featuredImg' onClick={() => setDestination("london")} />
             <div className="featuredTitles">
                 <h1>London</h1>
                 <h2>{data[2]} Properties</h2>
--- client/src/components/propertyList/PropertyList.jsx
+++ client/src/components/propertyList/PropertyList.jsx
@@ -6,6 +6,7 @@ export const PropertyList = () => {
 
     const {data, loading, error} = useFetch("/hotels/countByType");
 
+
     const images = [
         "https://cf.bstatic.com/xdata/images/hotel/square600/119307188.webp?k=fd0b2bfe19688da092671bbfd9a350e457aab99910cfac5f022bb5cc53d8c4dd&o=",
         "https://cf.bstatic.com/xdata/images/hotel/square600/296578071.webp?k=90e08e9466abac66c5582cada5d2ed99141ccc9f2ad99f674baf75a4b6302ab9&o=",
@@ -18,7 +19,7 @@ export const PropertyList = () => {
     <div className='pList'>
         {loading ? ("loading") :  (<> 
         {data && images.map((img, i) => (<div className="pListItem" key={i}>
-            <img src={img} alt="" className="pListImg" />
+            <img src={img} alt="" className="pListImg"/>
             <div className="pListTitles">
                 <h1>{data[i]?.type}</h1>
                 <h2>{data[i]?.count} {data[i]?.type}</h2>
