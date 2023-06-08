ace0ddd2f1764e3456323e2782c8fd134c7f21fa
Anton <56392640+AntonBertrand@users.noreply.github.com>
2023-06-08 15:00:03 +0100

    Update FeaturedProperties.jsx

--- client/src/components/featuredProperties/FeaturedProperties.jsx
+++ client/src/components/featuredProperties/FeaturedProperties.jsx
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom'
 
 export const FeaturedProperties = () => {
 
-    const {data, loading, error} = useFetch("/hotels?featured=true");
+    const {data, loading, error} = useFetch("https://booking-app-ue5a.onrender.com/api/hotels?featured=true");
 
   return (
     <div className='fp'>
