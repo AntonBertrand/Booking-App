d2bcb5ec9fcc7d883e8a2b530c26e382815a5b0b
Anton <56392640+AntonBertrand@users.noreply.github.com>
2023-08-09 13:11:02 +0100

    Update auth.js

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -32,14 +32,12 @@ export const login = async (req, res, next) => {
         const token = jwt.sign({id: user._id, isAdmin: user.isAdmin}, process.env.JWT);
 
         const {password, isAdmin, ...otherDetails} = user._doc;
-        res.cookie("access_token", token, {
+        return res.cookie("access_token", token, {
             httpOnly: true,
             sameSite: 'none',
             secure: true
-        })
-        .status(200)
-        .json({...otherDetails});
+        }).status(200).json({...otherDetails});
     } catch (err) {
-        next(err);
+        return res.status(400).json({error: err.message})
     }
 };
